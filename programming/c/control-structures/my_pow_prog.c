c5a074a388b9c5afbb0cc1e24f370ef42f61a737
jvela018 <jvela018@fiu.edu>
2018-06-27 11:46:02 +0300

    exponent

--- programming/c/control-structures/my_pow_prog.c
+++ programming/c/control-structures/my_pow_prog.c
@@ -15,7 +15,10 @@ int main(void)
 
     /* Add here a for loop that at each iteration prints out
        the current value of the power and updates val */
-#error Add the missing loop
+	for (n = 0; n <= N; n++) {
+        printf("\t %2d  \t  %5d\n", n, val);
+        val = val * k;
+    }
 
     return 0;
 }
