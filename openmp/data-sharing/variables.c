3f6513fc68821f8458b5aebe907f5c5a97189067
jvela018 <jvela018@fiu.edu>
2018-06-29 10:36:29 +0300

    openmpi exercises

--- openmp/data-sharing/variables.c
+++ openmp/data-sharing/variables.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main(void)
 {
     int var1 = 1, var2 = 2;
-
-    /* TODO:
-     *   Test the effect of different data sharing clauses here
-     */
-    {
-        printf("Region 1: var1=%i, var2=%i\n", var1, var2);
+    int omp_rank;	
+    
+#pragma omp parallel private(var1)
+    {	
+	int var1=50;
+	//omp_rank = omp_get_thread_num();
+        printf("Private Region 1: var1=%i, var2=%i\n", var1, var2);
         var1++;
         var2++;
     }
-    printf("After region 1: var1=%i, var2=%i\n\n", var1, var2);
+    printf("After region 1 (Private): var1=%i, var2=%i\n\n", var1, var2);
+
+#pragma omp parallel shared(var1)
+    {
+	int var1=2;
+	//omp_rank = omp_get_thread_num();
+        printf("Shared Region 1: var1=%i, var2=%i\n", var1, var2);
+        var1++;
+        var2++;
+    }
+    printf("After region 1 (Shared): var1=%i, var2=%i\n\n", var1, var2);
+
+
 
     return 0;
 }
