bbe6a713941d46cb9bbdd730a9211b789878f52f
jvela018 <jvela018@fiu.edu>
2018-06-30 14:19:21 +0300

    hello mpi

---
+++ mpi/hello-world/hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(void){
+
+//int rank;
+
+MPI_Init(NULL,NULL);
+
+int rank;
+int size; 
+
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+printf("Hello World!from process %d ", rank);
+
+if(rank==0){
+ printf("(size %d) \n", size);
+}else{
+ printf("\n");
+}
+
+//MPI_Finalize();
+
+return 0;
+}
