fd6fee7ff4dc97c7fe6a398afdf81188e0d29b73
jvela018 <jvela018@fiu.edu>
2018-06-30 16:05:41 +0300

    message exchange

--- mpi/message-exchange/exchange.c
+++ mpi/message-exchange/exchange.c
@@ -5,10 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    int i, myid, ntasks;
-    int size = 100;
+    int i, myid, ntasks, count, dest, source;
+    int size = 1000;
     int *message;
     int *receiveBuffer;
+        
+
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -25,6 +27,18 @@ int main(int argc, char *argv[])
 
     /* TODO: */
     /* Send and receive messages as defined in exercise */
+    count = sizeof(&message);
+    dest = ntasks-1;
+    source = 0; 
+    
+    if(myid==0){
+    MPI_Send(message      , count, MPI_INT, dest  , 1, MPI_COMM_WORLD);
+    }
+    if(myid == ntasks-1){
+    MPI_Recv(receiveBuffer, count, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    }	
+
+
     if (myid == 0) {
 
         printf("Rank %i received %i\n", myid, receiveBuffer[0]);
