d0f6cd958968147cde401271162470575388b21c
jvela018 <jvela018@fiu.edu>
2018-07-02 11:22:53 +0300

    user-defined datatypes

--- mpi/datatypes/c/custom_type_a-b.c
+++ mpi/datatypes/c/custom_type_a-b.c
@@ -5,7 +5,13 @@ int main(int argc, char **argv)
 {
     int rank;
     int array[8][8];
+    int a_count=8;
+    int a_blocklength=1;
+    int a_stride=8;
+    MPI_Status status;
+    
     //TODO: Declare a variable storing the MPI datatype
+    MPI_Datatype typea;
 
     int i, j;
 
@@ -37,14 +43,21 @@ int main(int argc, char **argv)
         }
     }
 
+
     //TODO: Create datatype that describes one column. Use MPI_Type_vector.
+      // Create datatype
+    MPI_Type_vector(a_count, a_blocklength, a_stride, MPI_INT, &typea);
+    MPI_Type_commit(&typea);
 
-    //TODO: Send first column of matrix form rank 0 to rank 1
+    // Send first column of matrix
+    if (rank == 0) {
+        MPI_Send(&array[0][1], a_blocklength, typea, 1, 1, MPI_COMM_WORLD);
+    } else if (rank == 1) {
+        MPI_Recv(&array[0][1], a_blocklength, typea, 0, 1, MPI_COMM_WORLD,
+                 &status);
+    }
 
-    //TODO: free datatype
-
-    // Print out the result on rank 1
-    // The application is correct if the first column has the values of rank 0
+    // Print out the result
     if (rank == 1) {
         printf("Received data\n");
         for (i = 0; i < 8; i++) {
@@ -55,6 +68,7 @@ int main(int argc, char **argv)
         }
     }
 
+    MPI_Type_free(&typea);
     MPI_Finalize();
 
     return 0;
